04e003524ce573445495eb29476835676f61f8f3
Davi Almeida <davistrife@live.com>
2021-05-31 22:59:46 -0300

    test: Should pedal must be created

---
+++ src/core/services/pedal/CreatePedal.spec.ts
@@ -0,0 +1,22 @@
+import { Pedal } from '@core/entities/Pedal'
+
+describe('Create Pedal', () => {
+  test('Should pedal must be created', () => {
+    // const pedalRepository = new PedalRepositoryMemory()
+    // const sut = new CreatePedalService(pedalRepository)
+
+    const params = {
+      name: '50KM do Lucas',
+      start_date: new Date('2021-11-12'),
+      start_date_registration: new Date('2021-10-12'),
+      end_date_registration: new Date('2021-10-31'),
+      additional_information: '',
+      start_place: 'Teresina',
+      participants_limit: '200'
+    }
+
+    const pedal = new Pedal(params)
+
+    expect(pedal).toBeInstanceOf(Pedal)
+  })
+})
