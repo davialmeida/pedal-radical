c28a2449a4ed0abbe768d7956ac3a81d31e0eeab
Davi Almeida <davistrife@live.com>
2021-05-31 22:09:05 -0300

    test: ensure AuthenticationService Should user must be authenticated

---
+++ src/core/services/authentication/Authentication.spec.ts
@@ -0,0 +1,40 @@
+import { BCryptEncrypt } from '@infra/encripters/bcrypt/BCryptEncrypt'
+import { UserRepositoryMemory } from '@infra/repository/UserRepositoryMemory'
+import { JWTToken } from '@infra/tokens/jsonwebtoken/JWTToken'
+import SignUpUserService from '../signup/SignUpUser'
+import { AuthenticationService } from './Authentication'
+
+describe('Authenticate User', () => {
+  test('Should user must be authenticated', async () => {
+    const userRepository = new UserRepositoryMemory()
+    const encripter = new BCryptEncrypt()
+    const tokenJWT = new JWTToken()
+
+    const signupService = new SignUpUserService(userRepository, encripter)
+
+    await signupService.execute({
+      name: 'Davi',
+      email: 'davistrife@live.com',
+      password: 'teste1234',
+      passwordConfirmation: 'teste1234'
+    })
+
+    const sut = new AuthenticationService(userRepository, encripter, tokenJWT)
+
+    const token = await sut.authenticate('davistrife@live.com', 'teste1234')
+
+    expect(typeof token).toEqual('string')
+  })
+
+  /* test('Should no register a user if email not provided', async () => {
+    const userRepository = new UserRepositoryMemory()
+    const encripter = new BCryptEncrypt()
+    const tokenJWT = new JWTToken()
+
+    const sut = new AuthenticationService(userRepository, encripter, tokenJWT)
+
+    const token = await sut.authenticate('davistrife@live.com', '')
+
+    expect(typeof token).toEqual('string')
+  }) */
+})
